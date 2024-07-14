15ef6aa3fecbb4b5d49c493f8d99a12d8f2f757b
luojunll <l3228414338@gmail.com>
2024-07-14 21:43:03 +0800

    修改个人介绍主页！

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -70,18 +70,12 @@ export default hopeTheme({
   // 主题功能选项：https://theme-hope.vuejs.press/zh/config/theme/feature.html
   blog: {
     articleInfo: ["Date", "PageView", "Category", "Tag", "ReadingTime"],
-    name: "清顺",
-    avatar: "/avatar_self.webp",
-    description: "迷信新工具，热衷于研究开源软件、心理学理论，定期分享探索成果",
+    name: "Luo",
+    avatar: "/avatar_self.png",
+    description: "爱自由、爱编程！",
     intro: "/intro.html",
     medias: {
-      GitHub: "https://github.com/rockbenben",
-      Zhihu: "https://www.zhihu.com/people/qingwhat",
-      少数派: { icon: "https://newzone.top/icons/sspai.svg", link: "https://sspai.com/u/zqj05i4v/posts" },
-      Wechat: "https://img.newzone.top/wechat.svg",
-      Email: "mailto:learndata@newzone.top",
-      Discord: "https://discord.gg/PZTQfJ4GjX",
-      RSS: "/rss.xml",
+      GitHub: "https://github.com/luojunll",
     },
   },
 
--- docs/intro.md
+++ docs/intro.md
@@ -4,4 +4,4 @@ title: 个人介绍
 icon: fa-solid fa-circle-nodes
 ---
 
-爱编程。
+爱自由、爱编程！
